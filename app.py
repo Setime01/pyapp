13a8cd382d656b07892764f98d67d82d29d90770
settime <setimetech@gmail.com>
2025-03-14 10:45:42 +0100

    version 2, added add.py

---
+++ app.py
@@ -0,0 +1,10 @@
+score = int(input('What is your score? '))
+
+if score > 80:
+    print("Excellent")
+elif score > 60:
+    print("Very Good")
+elif score > 40:
+    print("Good")
+elif score > 0:
+    print("Fair")
